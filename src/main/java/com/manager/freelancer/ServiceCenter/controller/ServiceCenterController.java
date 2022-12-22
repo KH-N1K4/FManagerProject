ded69587448f3f26779ce28269acdaac6a51fe11
jeonghyun <jhyeon310@naver.com>
2022-12-22 20:59:15 +0900

    ㅇㅇ

--- src/main/java/com/manager/freelancer/ServiceCenter/controller/ServiceCenterController.java
+++ src/main/java/com/manager/freelancer/ServiceCenter/controller/ServiceCenterController.java
@@ -1,4 +1,4 @@
-package com.manager.freelancer.ServiceCenter.controller;
+package com.manager.freelancer.serviceCenter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
