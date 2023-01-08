f6a27e7190a2a704650d8373f0beffec472e95f9
jeonghyun <jhyeon310@naver.com>
2023-01-09 01:03:40 +0900

    0109 필터수정

--- src/main/java/com/manager/freelancer/common/filter/ManagerFilter.java
+++ src/main/java/com/manager/freelancer/common/filter/ManagerFilter.java
@@ -13,15 +13,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.manager.freelancer.common.scheduling.LevelUPScheduling;
 import com.manager.freelancer.member.model.vo.Member;
 
 @WebFilter(filterName="managerFilter", // 필터 이름, 필터가 여러 개 존재할 때 순서 지정 시 사용
 			urlPatterns = {"/manager/*"} ) // 필터링한 요청 주소(패턴 가능)
 public class ManagerFilter implements Filter{
 	
+	
+	private Logger logger = LoggerFactory.getLogger(ManagerFilter.class);
+	
 	public void init(FilterConfig fConfig) throws ServletException {
 		// 필터 생성 시 수행
 		System.out.println("관리자 로그인 필터 생성");
+		logger.debug("*** : " + "관리자 로그인 필터 생성");
 	}
 
 	public void destroy() {
@@ -41,11 +49,13 @@ public class ManagerFilter implements Filter{
 		// -> session에 loginMember가 있는 지 확인
 		HttpSession session = req.getSession();
 		if(session.getAttribute("loginMember") == null) { // 로그인 X 상태
+			logger.debug("*** : " + "관리자 로그인 필터 생성 *****로그인 안됨");
 			resp.sendRedirect("/"); // 메인페이지로 redirect
 		} else { // 로그인 O 상태 
 			// 연결된 다음 필터로 이동(없으면 Servlet / JSP로 이동)
 			if(((Member)session.getAttribute("loginMember")).getAuthority()==1) {
 				resp.sendRedirect("/");
+				logger.debug("*** : " + "관리자 로그인 필터 생성 *****관리자 아님");
 			} else {
 				chain.doFilter(request, response);			
 				
